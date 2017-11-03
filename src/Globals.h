7bf40f48f270b2f9486f7ec3562bc45cba665a72
Bjørn Moholt <bjornmoh@ifi.uio.no>
2017-11-03 15:24:34 +0100

    works on linux again

--- src/Globals.h
+++ src/Globals.h
@@ -2,9 +2,9 @@
 
 #include <GL/glew.h>
 
-#include <GLFW\glfw3.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include <glm\gtc\matrix_transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 namespace Globals
 {
