76a46a824a50b910efae1fa90fb71f14c47a0c70
Bjørn Moholt <bjornmoh@ifi.uio.no>
2017-11-03 17:35:03 +0100

    ShaderBull

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "Globals.h"
 #include "ShaderProgram.h"
 
+#include"Sleep.h"
+
 int main(int argc, char* args[])
 {
     GLFWwindow* window;
@@ -29,7 +31,7 @@ int main(int argc, char* args[])
 	ShaderProgram* shader = new ShaderProgram();
     
     char* vertexShader = R"glsl(
-    #version 150 core
+    #version 130
 
     in vec3 position;
     out vec3 pos;
@@ -42,14 +44,20 @@ int main(int argc, char* args[])
     )glsl";
 
     char* fragmentShader = R"glsl(
-    #version 150 core
+    #version 130
+
+    uniform float t;
 
     in vec3 pos;
     out vec4 outColor;
 
     void main()
     {
-        outColor = vec4(pos.xy, 0.5 - sqrt(pos.x * pos.x + pos.y * pos.y)/10,1);
+        float n = sqrt((pos.x*pos.x)+(pos.y*pos.y))*10;
+
+        n += t;
+
+        outColor = vec4(tan(n),sin(n),cos(n),1);
     }
     )glsl";
 
@@ -58,9 +66,12 @@ int main(int argc, char* args[])
     shader->link();
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.0f,   0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
+        -100, -100, 0.0,
+         100,  100, 0.0,
+         100, -100, 0.0,
+        -100, -100, 0.0,
+        -100,  100, 0.0,
+         100,  100, 0.0,
     }; 
     
     GLuint vao, vbo = 0;
@@ -74,12 +85,20 @@ int main(int argc, char* args[])
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     shader->vertexAttribPointer("position", 3, GL_FLOAT, GL_FALSE, 0, 0);
+    int t_loc = shader->getUniformLocation("t");
+    
+    float t = 0;
 
     while(!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUniform1f(t_loc, t);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+        t += 0.3f;
+
+        sleep(1000/60.0f);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
